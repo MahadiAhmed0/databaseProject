3e3d2342929a48254ac535cb6f5725577b2faa1e
MahadiAhmed0 <mahadiahmed.2002@gmail.com>
2025-08-30 22:16:07 +0600

    first commit

---
+++ Frontend/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module main.ui_javafx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens main.ui_javafx to javafx.fxml;
+    exports main.ui_javafx;
+}
